bb76c855b0385f9eb9e24730684123541e50a4ae
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-06-20 15:23:59 -0300

    Update Ctrlviacep.js

--- __webcontent__/js/Ctrlviacep.js
+++ __webcontent__/js/Ctrlviacep.js
@@ -1,12 +1,35 @@
-app.controller('viacepctrl',['angularviacep'],
-			function ctrl($scope) {
-				$scope.address = {
-					zipcode : null,
-					street : null,
-					neighborhood : null,
-					uf : null,
-					city : null,
-					unit : null
-				}
-});
+(function($app) {
 
+	app.controller("CtrlViaCep", CtrlViaCep);
+	CtrlViaCep.$inject = ['$scope', '$http', '$q'];
+
+	function CtrlViaCep($scope, $http, $q) {
+
+		$scope.address = {
+			zipcode : null,
+			street : null,
+			neighborhood : null,
+			uf : null,
+			city : null,
+			unit : null
+		}
+
+		$scope.valor = null;
+		$scope.viacep = function() {
+			viaCepUrl = "https://viacep.com.br/ws/" + $scope.address.zipcode + "/json/";
+			deferred = $q.defer();
+			$http.get(viaCepUrl).then(function(response) {
+				var raw;
+				raw = response.data;
+				$scope.address = raw;
+				if (raw.erro) {
+					$scope.address = deferred.reject('CEP não encontrado');
+					return deferred.reject('CEP não encontrado');
+				} else {
+					return deferred.resolve(raw);
+				}
+			});
+		}
+	}
+
+}(app));
